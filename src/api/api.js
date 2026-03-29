4f32431e5ab9a6bfe60944e0397ff8a43ed72959
cucina656-dotcom <cucina656@gmail.com>
2026-03-29 09:20:36 +0000

    Hardcode worker URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL || '';
+const API_BASE = 'https://modekit.cucina656.workers.dev';
 
 async function apiCall(endpoint, options = {}) {
   const url = `${API_BASE}${endpoint}`;
